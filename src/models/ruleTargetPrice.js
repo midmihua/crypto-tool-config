678d6b56ca8cb782d6fbe4207173ebd829abab45
dmytro.mykhailenko <mdmprog@gmail.com>
2018-11-26 00:25:34 +0200

    model fixed

--- src/models/ruleTargetPrice.js
+++ src/models/ruleTargetPrice.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TargetPriceSchema = new Schema({
-    info: { type: String, default: 'target_price rule' },
-    user: { type: String, require: true },
+    name: { type: String, default: 'target_price', required: true },
+    user: { type: String, required: true },
     params: {
-        market: { type: String, require: true },
-        symbol: { type: String, require: true },
-        targetPrice: { type: Number, require: true },
-        direction: { type: String, enum: ['>=', '<='], default: '>=', require: true },
-        retry: { type: Number, default: 1, require: true }
+        market: { type: String, required: true },
+        symbol: { type: String, required: true },
+        targetPrice: { type: Number, required: true },
+        direction: { type: String, enum: ['>=', '<='], default: '>=', required: true },
+        retry: { type: Number, default: 1, required: true }
     },
     sendTo: {
         token: String,
         chatId: String
     },
-    status: { type: String, enum: ['A', 'D'], default: 'A', require: true },
+    status: { type: String, enum: ['A', 'D'], default: 'A', required: true },
     createDate: { type: Date, default: Date.now }
 });
 
